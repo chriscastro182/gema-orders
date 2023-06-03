faf6735d3f175aad1cd61df30608bafd3844f723
Christian <deck.chris.182@gmail.com>
2023-06-03 13:25:13 -0600

    verification for duplicate applicated

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -1,11 +1,15 @@
 import { Router } from "express"
+import { authJwt, verifySignUp } from '../middlewares'
 const router = Router()
 
 import * as authCtrl from '../controllers/auth.controller'
 
 router.post('/signin', authCtrl.signIn)
 
-router.post('/signup', authCtrl.signUp)
+router.post('/signup', [
+    verifySignUp.checkRolesExisted,
+    verifySignUp.checkDuplicateUser
+], authCtrl.signUp)
 
 
 export default router
